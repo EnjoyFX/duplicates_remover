3a797ded70ab9364c5c3f05b9763dfe6113fb466
AndyFX <vba2you@gmail.com>
2023-10-22 19:44:18 +0300

    adding unit tests

---
+++ tests/test_duplicates_remover.py
@@ -0,0 +1,59 @@
+import unittest
+import os
+
+from unittest.mock import patch
+from tempfile import TemporaryDirectory as temp_dir
+from duplicates_remover import DuplicatesRemover
+
+
+class TestDuplicatesRemover(unittest.TestCase):
+
+    def setUp(self):
+        self.duplicates_remover = DuplicatesRemover()
+
+    def test_delete_duplicate_files(self):
+        # Create temp dirs with test files
+        with temp_dir() as source_dir, temp_dir() as target_dir:
+            # Create test files in the source and target directories
+            source_file1 = os.path.join(source_dir, "file1.txt")
+            target_file1 = os.path.join(target_dir, "file1.txt")
+            source_file2 = os.path.join(source_dir, "file2.txt")
+            target_file2 = os.path.join(target_dir, "file2.txt")
+
+            # Creating  a pair of files with same cintent
+            # and another pair - with different content:
+            content = "Same content"
+            with open(source_file1, "w") as f:
+                f.write(content)
+            with open(target_file1, "w") as f:
+                f.write(content)
+
+            with open(source_file2, "w") as f:
+                f.write(content+content)
+            with open(target_file2, "w") as f:
+                f.write(content+content+content)
+
+            # Mock the logger to capture log messages
+            with patch('duplicates_remover.logger') as mock_logger:
+                result = self.duplicates_remover.delete_duplicate_files(
+                    source_dir, target_dir, deep_check=True)
+
+                # Checking the 'result' variable is correct
+                self.assertEqual(result["compared"], 2)
+                self.assertEqual(result["deleted"], 1)
+
+                # Checking logs
+                mock_logger.info.assert_called_with(
+                    f"File {target_file1} removed OK..."
+                    f"(same as {source_file1})")
+
+                # Ensure that the duplicate file in target removed
+                self.assertFalse(os.path.exists(target_file1))
+                self.assertTrue(os.path.exists(target_file2))
+
+                # Checking warnings if any
+                self.assertFalse(mock_logger.warning.called)
+
+
+if __name__ == '__main__':
+    unittest.main()
